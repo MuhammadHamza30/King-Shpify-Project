6f6089e9cf501273cab391bb2f4c412c0eed53df
Muhammad Hamza <132424951+MuhammadHamza30@users.noreply.github.com>
2024-11-05 07:34:45 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,3 @@
-// import './App.css'
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// function App() {
-
-//   return (
-
-//     <BrowserRouter>
-//     <Routes>
-//       <Route index="/" element={<Home />} />
-//     </Routes>
-//   </BrowserRouter>
-//   )
-// }
-
-// export default App
-
-
-// import * as React from "react";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import "./App.css";
-
-// function App(){
-
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element:<Home/>,
-//     },
-//   ]);
-
-//   return (
-
-//         <RouterProvider router={router} />
-
-//     )
-//   }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../src/pages/Home.jsx'
@@ -59,12 +15,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index="/" element={<Home />} />
-        <Route path="/Shop" element={<Shop />} />
-        <Route path="/NewArrivals" element={<NewArrivals />} />
-        <Route path="/Collection" element={<Collection />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SigUp" element={<SigUp />} />
+        <Route path="./Shop" element={<Shop />} />
+        <Route path="./NewArrivals" element={<NewArrivals />} />
+        <Route path="./Collection" element={<Collection />} />
+        <Route path="./About" element={<About />} />
+        <Route path="./Login" element={<Login />} />
+        <Route path="./SigUp" element={<SigUp />} />
       </Routes>
     </BrowserRouter>
 
